d914c414c9161f768fd56326cba7dd78c2444cd8
Shaq383 <shaquilfathzanasution@students.polmed.ac.id>
2023-10-04 09:23:09 +0700

    Change in description
    adding name

--- app/src/main/java/com/example/taskone/MainActivity.kt
+++ app/src/main/java/com/example/taskone/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             TaskOneTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("Android, My name Shaquil Fathza Nasution")
                 }
             }
         }
@@ -38,6 +38,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TaskOneTheme {
-        Greeting("Android")
+        Greeting("Android, My name Shaquil Fathza Nasution")
     }
 }
